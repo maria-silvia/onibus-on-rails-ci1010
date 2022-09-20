74fdec43adbda86ca35e5907cd0a396bedaec423
Maria Silvia <mariasrr97@gmail.com>
2022-09-20 15:25:57 -0300

    color linhas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def get_color_by_tipo(tipo)
+        if tipo.cor == "amarelo"
+            return 'rgb(233, 233, 66)'
+        elsif tipo.cor == 'vermelho'
+            return "red"
+        elsif tipo.cor == 'cinza'
+            return "gray"
+        elsif tipo.cor == 'verde'
+            return "green"
+        else    
+            return 'black'
+        end
+    end
 end
